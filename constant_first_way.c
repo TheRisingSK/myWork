b70e45ce29e05e77fd9624c88cf674adc1b3916f
santosh kshirsagar <22586175+santoshk51@users.noreply.github.com>
2018-06-07 03:00:58 +0530

    Add files via upload

---
+++ constant_first_way.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//define constants
+#define LENGTH 10
+#define WIDTH 4
+
+int main(){
+
+    int area;
+
+    area = LENGTH * WIDTH;
+    printf("Area : %d", area);
+
+    return 0;
+}
+/*
+Area : 40
+*/
