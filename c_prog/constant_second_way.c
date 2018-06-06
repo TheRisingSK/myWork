0eec71fef860e1d0249d4bfbb33cd3b2dc15ef22
santosh kshirsagar <22586175+santoshk51@users.noreply.github.com>
2018-06-07 03:02:23 +0530

    define constants using const keyword

---
+++ c_prog/constant_second_way.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    //define constants
+    const int LENGTH = 10;
+    const int WIDTH = 4;
+
+    int area;
+
+    area = LENGTH * WIDTH;
+    printf("Area : %d", area);
+
+    return 0;
+}
+/*
+Area : 40
+*/
