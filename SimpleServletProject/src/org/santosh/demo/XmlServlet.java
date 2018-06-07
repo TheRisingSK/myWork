c0f6bdc8bfbf7c5e66d531ef6e456674e9a61bf3
santosh <santosh@192.168.0.9>
2018-06-08 01:23:57 +0530

    Demo Servlet Get and Post Methods

---
+++ SimpleServletProject/src/org/santosh/demo/XmlServlet.java
@@ -0,0 +1,34 @@
+package org.santosh.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XmlServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();			
+			out.println("Hello from GET : ");			
+			String username = request.getParameter("userName");
+			String fullname = request.getParameter("fullName");
+			out.print(username + " " + fullname);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();			
+			out.println("Hello from POST : ");			
+			String username = request.getParameter("userName");
+			String fullname = request.getParameter("fullName");
+			out.print(username + " " + fullname);
+	}
+}
